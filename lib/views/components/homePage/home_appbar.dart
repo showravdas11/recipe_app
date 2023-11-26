fba380cf5661b3c1bd8e05c6ae34faec6f104a14
showravdas11 <showravdas11@gmail.com>
2023-11-27 02:54:47 +0600

    google sign 70% done and setting page changes

--- lib/views/components/homePage/home_appbar.dart
+++ lib/views/components/homePage/home_appbar.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:green_recipe/common/widgets/toast/toast.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:green_recipe/features/authentication/controllers/user_controller.dart';
 import 'package:green_recipe/views/screens/login_screen/login.dart';
 
 class HomeAppbar extends StatefulWidget {
@@ -12,32 +14,43 @@ class HomeAppbar extends StatefulWidget {
   State<HomeAppbar> createState() => _HomeAppbarState();
 }
 
+const colorizeColors = [
+  Color.fromARGB(255, 0, 0, 0),
+  Color.fromARGB(255, 150, 191, 13),
+  Color.fromARGB(255, 143, 137, 19),
+  Color.fromARGB(255, 214, 108, 9)
+];
+const colorizeTextStyle =
+    TextStyle(fontSize: 32, fontWeight: FontWeight.bold, height: 1);
+
 class _HomeAppbarState extends State<HomeAppbar> {
   final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Text(
-          "What are you\ncooking today?",
-          style:
-              TextStyle(fontSize: 32, fontWeight: FontWeight.bold, height: 1),
+        AnimatedTextKit(
+          repeatForever: true,
+          animatedTexts: [
+            ColorizeAnimatedText(
+              'What are you\ncooking today?',
+              textStyle: colorizeTextStyle,
+              colors: colorizeColors,
+            ),
+            // ColorizeAnimatedText(
+            //   'cooking today?',
+            //   textStyle: colorizeTextStyle,
+            //   colors: colorizeColors,
+            // ),
+          ],
+          isRepeatingAnimation: true,
+          onTap: () {
+            print("Tap Event");
+          },
         ),
         const Spacer(),
-        Card(
-          child: IconButton(
-              onPressed: () {
-                auth.signOut().then((value) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ));
-                }).onError((error, stackTrace) {
-                  Utils.toastMessage(error.toString());
-                });
-              },
-              icon: const Icon(Icons.logout_outlined)),
+        CircleAvatar(
+          foregroundImage: NetworkImage(UserController.user?.photoURL ?? ''),
         )
       ],
     );
